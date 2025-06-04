393bf5bdbe1035dcb916c3a7123cc6c3ce06d39c
Kvazar-213452 <kvazar382@gmail.com>
2025-06-05 00:37:10 +0300

    fix

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 https://simplemaps.com/svg/country/ua
 https://simplemaps.com/custom/country#data
 https://gadm.org/download_country.html
+
+https://www.pixel-stitch.net/
--- app/web/src/web/public/js/main.js
+++ app/web/src/web/public/js/main.js
@@ -1,29 +1,97 @@
-// main.js
+Promise.all([
+    fetch('/geojson/gadm41_UKR_1.json').then(res => res.json()), // області
+    fetch('/geojson/gadm41_UKR_2.json').then(res => res.json())  // райони
+]).then(([oblastsData, districtsData]) => {
+    const container = document.getElementById('map-container');
+    const width = container.clientWidth;
+    const height = container.clientHeight;
 
-$(document).ready(function () {
-    load_chat();
+    const svg = d3.select("#map")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-    // const $textarea = $('.input_message textarea');
+    // Оптимальна проекція для України
+    const projection = d3.geoMercator()
+        .center([31.1656, 48.3794])
+        .scale(Math.min(width, height) * 4.5) // Збільшений масштаб
+        .translate([width / 2, height / 2]);
 
-    // $textarea.on('input', function () {
-    //     $(this).height('auto');
+    const path = d3.geoPath().projection(projection);
 
-    //     const scrollHeight = this.scrollHeight;
-    //     const maxHeight = $(window).height() - 400;
+    // Відображення районів
+    svg.selectAll("path.region")
+        .data(districtsData.features)
+        .enter()
+        .append("path")
+        .attr("class", "region")
+        .attr("d", path);
 
-    //     if (scrollHeight < maxHeight) {
-    //         $(this).css({
-    //             'overflow-y': 'hidden',
-    //             'height': scrollHeight + 'px'
-    //         });
-    //     } else {
-    //         $(this).css({
-    //             'overflow-y': 'auto',
-    //             'height': maxHeight + 'px'
-    //         });
-    //     }
-    // });
+    // Контури областей
+    svg.selectAll("path.oblast-border")
+        .data(oblastsData.features)
+        .enter()
+        .append("path")
+        .attr("class", "oblast-border")
+        .attr("d", path);
+
+    // Розрахунок позицій для підписів областей
+    const oblasts = {};
+    districtsData.features.forEach(feature => {
+        const oblastName = feature.properties.NAME_1;
+        if (!oblasts[oblastName]) {
+            oblasts[oblastName] = {
+                count: 1,
+                centroid: path.centroid(feature)
+            };
+        } else {
+            oblasts[oblastName].count++;
+            oblasts[oblastName].centroid[0] += path.centroid(feature)[0];
+            oblasts[oblastName].centroid[1] += path.centroid(feature)[1];
+        }
+    });
+
+    // Нормалізація позицій
+    Object.keys(oblasts).forEach(oblast => {
+        oblasts[oblast].centroid[0] /= oblasts[oblast].count;
+        oblasts[oblast].centroid[1] /= oblasts[oblast].count;
+    });
+
+    // Додавання підписів областей
+    svg.selectAll("text.oblast-label")
+        .data(Object.keys(oblasts))
+        .enter()
+        .append("text")
+        .attr("class", "oblast-label")
+        .attr("x", d => oblasts[d].centroid[0])
+        .attr("y", d => oblasts[d].centroid[1])
+        .text(d => d);
+
+    // Адаптація до зміни розміру вікна
+    window.addEventListener('resize', () => {
+        const newWidth = container.clientWidth;
+        const newHeight = container.clientHeight;
+        
+        svg.attr("width", newWidth)
+            .attr("height", newHeight);
+        
+        projection.scale(Math.min(newWidth, newHeight) * 4.5)
+                    .translate([newWidth / 2, newHeight / 2]);
+        
+        svg.selectAll("path").attr("d", path);
+        svg.selectAll("text.oblast-label")
+            .attr("x", d => oblasts[d].centroid[0])
+            .attr("y", d => oblasts[d].centroid[1]);
+    });
+}).catch(error => {
+    console.error("Помилка завантаження даних:", error);
+    document.getElementById('map').innerHTML = 
+        '<div style="color:red;text-align:center;padding-top:300px">Помилка завантаження картографічних даних</div>';
 });
+
+
+
+
 
 function load_chat() {
     $.ajax({
